9bdd9f63a650d8e22b3fac953e944750f48ff67f
Philip Peter <Philip.Peter@justgeek.de>
2019-03-28 18:20:02 +0100

    Changes content description file to yml from json

--- generate_pdf.rb
+++ generate_pdf.rb
@@ -2,41 +2,76 @@
 
 require "prawn"
 require "json"
+require "yaml"
 
 
+def title_page(pdf)
+  pdf.text "Simple Sketchbook", :valign => :center, :align => :center, :size => 20
+end
 
-Prawn::Document.generate("sketchbook.pdf", :page_size => "A6") do
+class SinglePageLeft
+  @@box_width = 100
+  @@image_size = 50
 
-  text "Simple Sketchbook", :valign => :center, :align => :center, :size => 20
+  def initialize(description, image, margin_box)
+    @description = description
+    @image = image
 
-
-  json_string = File.read "content.json"
-  content = JSON.parse(json_string)
-  content['pages'].each do | page |
-    start_new_page
-
-    box_width = 100
-    image_size = 50
-
-    text_right_corner = margin_box.width - box_width
-    image_right_corner = margin_box.width - image_size
-    is_even = page_number % 2 == 0
-
-    text_x = if is_even then text_right_corner.round else 0 end
-    image_x = if is_even then image_right_corner.round else 0 end
-    alignment = if is_even then :right else :left end
     image_y = margin_box.height
-    text_y = image_y - image_size
 
-    image "images/#{page['image']}",
-          :at => [image_x, image_y],
-          :width => image_size,
-          :height => image_size
+    @image_position = [0, image_y]
+    @text_position = [0, image_y - @@image_size]
+    @alignment = :left
+  end
 
-    text_box page['description'],
-                 :at => [text_x, text_y],
-                 :align  => alignment,
-                 :width  => box_width,
-                 :size => 8
+  def render(pdf)
+    pdf.start_new_page
+
+    pdf.image "images/#{@image}",
+          :at => @image_position,
+          :width => @@image_size,
+          :height => @@image_size
+
+    pdf.text_box @description,
+             :at => @text_position,
+             :align => @alignment,
+             :width => @@box_width,
+             :size => 8
+  end
+end
+
+class SinglePageRight < SinglePageLeft
+  def initialize(description, image, margin_box)
+    @description = description
+    @image = image
+
+    image_y = margin_box.height
+    image_x = margin_box.width - @@image_size
+    text_x = margin_box.width - @@box_width
+
+    @image_position = [image_x, image_y]
+    @text_position = [text_x, image_y - @@image_size]
+    @alignment = :right
+  end
+
+end
+
+
+Prawn::Document.generate("sketchbook.pdf", :page_size => "A6") do |pdf|
+
+  title_page(pdf)
+
+
+  content = YAML.load_file('content.yml')
+  content['pages'].each do | page |
+    description = page['description']
+    image_name = page['image']
+
+    page = if pdf.page_number % 2 == 0 then
+               SinglePageLeft.new(description, image_name, pdf.margin_box)
+             else
+               SinglePageRight.new(description, image_name, pdf.margin_box)
+           end
+    page.render(pdf)
   end
 end
